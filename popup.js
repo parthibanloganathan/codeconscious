c56e7f0c405e4a76444e30bf2e40c6a0245957ee
Aditya Majumdar <aditya29@gmail.com>
2013-09-08 00:00:25 -0400

    now links to venmo

---
+++ popup.js
@@ -0,0 +1,14 @@
+  function test()
+  {
+    alert("worked");
+  }
+
+jQuery(document).ready(function(){
+
+  CLIENT_ID = "1366";
+  jQuery("#venmo_sign_in").click(function() {
+    console.log('venmo clicked');
+    window.open("https://api.venmo.com/oauth/authorize?client_id=" + CLIENT_ID + "&scope=make_payments&response_type=token");
+  });
+
+});
