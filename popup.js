4991da621200611997ee752f51b49d2d0d244ee8
Derek Xingzhou He <derekhe@gmail.com>
2013-09-08 02:09:49 -0400

    rename

--- popup.js
+++ popup.js
@@ -8,13 +8,11 @@ jQuery(document).ready(function(){
   CLIENT_ID = "1366";
   jQuery("#venmo_sign_in").click(function() {
       venmoAuth.authorize(function(){
-          chrome.storage.sync.set({'venmoToken': venmoAuth.getAccessToken()});
           });
   });
 
   jQuery("#github_sign_in").click(function() {
       githubAuth.authorize(function(){
-          chrome.storage.sync.set({'githubToken': githubAuth.getAccessToken()});
           });
   });
 });
