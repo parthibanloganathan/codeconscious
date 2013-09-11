562732c756a56bfe04d0c867a9e30fca317723bb
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2013-09-10 21:09:56 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
+// Create a GitHub app to get a client id and secret. See https://github.com/settings/applications/new
 var githubAuth = new OAuth2('github', {
-    client_id: '',
-    client_secret: '',
+    client_id: 'INSERT CLIENT ID FROM GITHUB HERE',
+    client_secret: 'INSERT CLIENT SECRET FROM GITHUB HERE',
     api_scope: 'user, repo'
 });
 
+// Create a Venmo app to get a client id and secret. See https://beta-developer.venmo.com/authentication
 var venmoAuth = new OAuth2('venmo', {
-    client_id: '',
-    client_secret: '',
+    client_id: 'INSERT CLIENT ID FROM VENMO HERE',
+    client_secret: 'INSERT CLIENT SECRET FROM VENMO HERE',
     api_scope: 'access_profile, access_friends, make_payments'
 });
