ef3847ab8bcfce4cdc86e5e4386bb5aceb0d16be
Derek Xingzhou He <derekhe@gmail.com>
2013-09-10 20:58:38 -0400

    Deleted api keys

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 var githubAuth = new OAuth2('github', {
-    client_id: '85fe67036c98989b1f32',
-    client_secret: '4bebfb368050da446cc481989b1978fd87aab9e4',
+    client_id: '',
+    client_secret: '',
     api_scope: 'user, repo'
 });
 
 var venmoAuth = new OAuth2('venmo', {
-    client_id: '1364',
-    client_secret: 'k6S6nyNezuACYW8DayRtfbLGnDhMJ6VJ',
+    client_id: '',
+    client_secret: '',
     api_scope: 'access_profile, access_friends, make_payments'
 });
